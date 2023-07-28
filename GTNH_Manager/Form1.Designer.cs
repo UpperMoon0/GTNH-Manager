aea2254314296ac62ec916fe2fdd0026737a136f
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-07-28 14:03:59 +0700

    Version 0.1

--- GTNH_Manager/Form1.Designer.cs
+++ GTNH_Manager/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GTNH_Manager
 {
-    partial class Form1
+    partial class GTNHManager
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            versionListBox = new ListBox();
+            chooseVersionLabel = new Label();
+            downloadButton = new Button();
+            multiMCPathTextBox = new TextBox();
+            multiMCPathLabel = new Label();
+            progressBar = new ProgressBar();
+            progressLabel = new Label();
+            statusLabel = new Label();
+            optifineCheckBox = new CheckBox();
+            SuspendLayout();
+            // 
+            // versionListBox
+            // 
+            versionListBox.FormattingEnabled = true;
+            versionListBox.ItemHeight = 15;
+            versionListBox.Location = new Point(12, 186);
+            versionListBox.Name = "versionListBox";
+            versionListBox.Size = new Size(120, 94);
+            versionListBox.TabIndex = 0;
+            // 
+            // chooseVersionLabel
+            // 
+            chooseVersionLabel.AutoSize = true;
+            chooseVersionLabel.Location = new Point(12, 168);
+            chooseVersionLabel.Name = "chooseVersionLabel";
+            chooseVersionLabel.Size = new Size(97, 15);
+            chooseVersionLabel.TabIndex = 1;
+            chooseVersionLabel.Text = "Choose a version";
+            // 
+            // downloadButton
+            // 
+            downloadButton.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            downloadButton.Location = new Point(12, 27);
+            downloadButton.Name = "downloadButton";
+            downloadButton.Size = new Size(201, 94);
+            downloadButton.TabIndex = 2;
+            downloadButton.Text = "Install";
+            downloadButton.UseVisualStyleBackColor = true;
+            downloadButton.Click += downloadButton_Click;
+            // 
+            // multiMCPathTextBox
+            // 
+            multiMCPathTextBox.Location = new Point(12, 142);
+            multiMCPathTextBox.Name = "multiMCPathTextBox";
+            multiMCPathTextBox.Size = new Size(207, 23);
+            multiMCPathTextBox.TabIndex = 3;
+            // 
+            // multiMCPathLabel
+            // 
+            multiMCPathLabel.AutoSize = true;
+            multiMCPathLabel.Location = new Point(12, 124);
+            multiMCPathLabel.Name = "multiMCPathLabel";
+            multiMCPathLabel.Size = new Size(81, 15);
+            multiMCPathLabel.TabIndex = 4;
+            multiMCPathLabel.Text = "MultiMC path";
+            // 
+            // progressBar
+            // 
+            progressBar.Location = new Point(219, 27);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new Size(569, 23);
+            progressBar.TabIndex = 5;
+            // 
+            // progressLabel
+            // 
+            progressLabel.AutoSize = true;
+            progressLabel.Location = new Point(219, 53);
+            progressLabel.Name = "downloadProgressLabel";
+            progressLabel.Size = new Size(0, 15);
+            progressLabel.TabIndex = 6;
+            // 
+            // statusLabel
+            // 
+            statusLabel.AutoSize = true;
+            statusLabel.Location = new Point(219, 68);
+            statusLabel.Name = "statusLabel";
+            statusLabel.Size = new Size(26, 15);
+            statusLabel.TabIndex = 7;
+            statusLabel.Text = "Idle";
+            // 
+            // optifineCheckBox
+            // 
+            optifineCheckBox.AutoSize = true;
+            optifineCheckBox.Location = new Point(142, 186);
+            optifineCheckBox.Name = "optifineCheckBox";
+            optifineCheckBox.Size = new Size(103, 19);
+            optifineCheckBox.TabIndex = 8;
+            optifineCheckBox.Text = "Install Optifine";
+            optifineCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // GTNHManager
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(optifineCheckBox);
+            Controls.Add(statusLabel);
+            Controls.Add(progressLabel);
+            Controls.Add(progressBar);
+            Controls.Add(multiMCPathLabel);
+            Controls.Add(multiMCPathTextBox);
+            Controls.Add(downloadButton);
+            Controls.Add(chooseVersionLabel);
+            Controls.Add(versionListBox);
+            Name = "GTNHManager";
+            Text = "GTNH Manager " + version;
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private ListBox versionListBox;
+        private Label chooseVersionLabel;
+        private Button downloadButton;
+        private TextBox multiMCPathTextBox;
+        private Label multiMCPathLabel;
+        private ProgressBar progressBar;
+        private Label progressLabel;
+        private Label statusLabel;
+        private CheckBox optifineCheckBox;
     }
 }
--- GTNH_Manager/Form1.cs
+++ GTNH_Manager/Form1.cs
@@ -1,10 +1,321 @@
+using System;
+using System.Diagnostics;
+using System.IO.Compression;
+using System.Net;
+using Ionic.Zip;
+using static System.ComponentModel.Design.ObjectSelectorEditor;
+
 namespace GTNH_Manager
 {
-    public partial class Form1 : Form
+    public partial class GTNHManager : Form
     {
-        public Form1()
+        string version = "0.1";
+        public GTNHManager()
         {
             InitializeComponent();
+            versionListBox.Items.Add("2.3.5 BETA");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        // This method is called when the downloadButton is clicked
+        private async void downloadButton_Click(object sender, EventArgs e)
+        {
+            // Check if the path specified in the multiMCPathTextBox is valid
+            if (IsMultiMCPathValid() && versionListBox.SelectedItem != null)
+            {
+                // Check if an item is selected in the versionListBox and if the selected item is "2.3.5 BETA"
+                if (versionListBox.SelectedItem != null && versionListBox.SelectedItem.ToString() == "2.3.5 BETA")
+                {
+                    // Set the URL of the file to download
+                    string url = "https://www.dropbox.com/scl/fi/r5ohaxwfzvyrphx2bn6zj/GT_New_Horizons_2.3.5_Client_Java_17-20.zip?dl=1&rlkey=spfjszkg02nrej3eqds42r0cg";
+                    // Set the path of the temporary folder
+                    string tempPath = Path.GetTempPath();
+                    // Set the path of the file to save in the temporary folder
+                    string filePath = Path.Combine(tempPath, "GT_New_Horizons_2.3.5_Client_Java_17-20.zip");
+                    // Update the statusLabel text and color
+                    UpdateStatusLabel("Downloading the modpack", Color.Blue);
+
+                    // Create a new HttpClient to download the file
+                    using (HttpClient client = new HttpClient())
+                    {
+                        // Send a GET request to the specified URL
+                        HttpResponseMessage response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead);
+                        // Check if the response was successful
+                        if (response.IsSuccessStatusCode)
+                        {
+                            // Get the content length of the response
+                            long? contentLength = response.Content.Headers.ContentLength;
+                            // Get the content stream of the response
+                            using (Stream stream = await response.Content.ReadAsStreamAsync())
+                            // Create a new FileStream to save the content to a file
+                            using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+                            {
+                                // Download the file and track its progress
+                                await InstallFile(stream, fileStream, contentLength);
+                            }
+                        }
+                    }
+                    progressLabel.Text = "";
+                    UpdateStatusLabel("Installation complete", Color.Green);
+                }
+            }
+            if (!IsMultiMCPathValid() && versionListBox.SelectedItem == null)
+                UpdateStatusLabel("Invalid MultiMC path and version", Color.Red);
+            else if (!IsMultiMCPathValid())
+                UpdateStatusLabel("Invalid MultiMC path", Color.Red);
+            else if (versionListBox.SelectedItem == null)
+                UpdateStatusLabel("Select a version", Color.Red);   
+        }
+
+        // This method checks if the path specified in the multiMCPathTextBox is valid
+        private bool IsMultiMCPathValid()
+        {
+            // Check if the directory exists at the specified path
+            if (Directory.Exists(multiMCPathTextBox.Text))
+                return true;
+            else
+                return false;
+        }
+
+        // This method updates the text and color of the statusLabel
+        private void UpdateStatusLabel(string text, Color color)
+        {
+            statusLabel.ForeColor = color;
+            statusLabel.Text = text;
+        }
+
+        //Download and install the file
+        private async Task InstallFile(Stream stream, FileStream fileStream, long? contentLength)
+        {
+            int bufferSize = 8192;
+            byte[] buffer = new byte[bufferSize];
+            long totalBytesRead = 0;
+            int bytesRead;
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            while ((bytesRead = await stream.ReadAsync(buffer, 0, bufferSize)) != 0)
+            {
+                await fileStream.WriteAsync(buffer, 0, bytesRead);
+                totalBytesRead += bytesRead;
+                UpdateProgress(totalBytesRead, contentLength, stopwatch);
+            }
+
+            // Ensure that the FileStream is properly disposed of before attempting to extract its contents
+            fileStream.Dispose();
+
+            // Check if the download is complete
+            if (totalBytesRead == contentLength)
+            {
+                // Update the statusLabel text and color
+                UpdateStatusLabel("Creating Instance Folder", Color.Blue);
+                // Update the statusLabel text and color
+                UpdateStatusLabel("Extracting Downloaded File", Color.Blue);
+                // Extract the contents of the downloaded file to the instance folder
+                string instancePath = ExtractDownloadedFile(fileStream.Name);
+                // Install optifine
+                if (optifineCheckBox.Checked)
+                {
+                    UpdateStatusLabel("Downloading Optifine", Color.Blue);
+                    string optifineDownloadUrl = "https://drive.google.com/u/0/uc?id=1nR5ob2lm9RTMiczTlKiq5jCvvIFrhM_7&export=download";
+                    string optifineFilePath = Path.Combine(instancePath, ".minecraft", "mods", "OptiFine.jar");
+                    using (HttpClient client = new HttpClient())
+                    {
+                        HttpResponseMessage response = await client.GetAsync(optifineDownloadUrl);
+                        response.EnsureSuccessStatusCode();
+                        using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
+                                opFileStream = new FileStream(optifineFilePath, FileMode.Create, FileAccess.Write, FileShare.None, bufferSize, true))
+                        {
+                            totalBytesRead = 0;
+                            contentLength = response.Content.Headers.ContentLength;
+                            stopwatch.Restart();
+                            while ((bytesRead = await contentStream.ReadAsync(buffer, 0, bufferSize)) != 0)
+                            {
+                                await opFileStream.WriteAsync(buffer, 0, bytesRead);
+                                totalBytesRead += bytesRead;
+                                UpdateProgress(totalBytesRead, contentLength, stopwatch);
+                            }
+                        }
+                    }
+                }
+                // Change pack settings
+                ChangePackSetting(instancePath);
+            }
+        }
+
+
+        //Change the pack setting
+        private void ChangePackSetting(string instancePath)
+        {
+            // Write to instance.cfg
+            string instanceCfgPath = Path.Combine(instancePath, "instance.cfg");
+            string[] instanceCfgLines = new string[]
+            {
+                "InstanceType=OneSix",
+                "JavaPath=javaw",
+                "JoinServerOnLaunch=false",
+                "JvmArgs=--illegal-access=warn -Djava.security.manager=allow -Dfile.encoding=UTF-8 --add-opens java.base/jdk.internal.loader=ALL-UNNAMED --add-opens java.base/java.net=ALL-UNNAMED --add-opens java.base/java.nio=ALL-UNNAMED --add-opens java.base/java.io=ALL-UNNAMED --add-opens java.base/java.lang=ALL-UNNAMED --add-opens java.base/java.lang.reflect=ALL-UNNAMED --add-opens java.base/java.text=ALL-UNNAMED --add-opens java.base/java.util=ALL-UNNAMED --add-opens java.base/jdk.internal.reflect=ALL-UNNAMED --add-opens java.base/sun.nio.ch=ALL-UNNAMED --add-opens jdk.naming.dns/com.sun.jndi.dns=ALL-UNNAMED,java.naming --add-opens java.desktop/sun.awt.image=ALL-UNNAMED --add-modules jdk.dynalink --add-opens jdk.dynalink/jdk.dynalink.beans=ALL-UNNAMED --add-modules java.sql.rowset --add-opens java.sql.rowset/javax.sql.rowset.serial=ALL-UNNAMED",
+                "MaxMemAlloc=8192",
+                "MinMemAlloc=6144",
+                "OverrideCommands=false",
+                "OverrideConsole=false",
+                "OverrideGameTime=false",
+                "OverrideJavaArgs=true",
+                "OverrideJavaLocation=true",
+                "OverrideMemory=true",
+                "OverrideNativeWorkarounds=false",
+                "OverrideWindow=false",
+                "PermGen=128",
+                "iconKey=default",
+                "name=" + versionListBox.SelectedItem.ToString(),
+                "notes="
+            };
+            File.WriteAllLines(instanceCfgPath, instanceCfgLines);
+
+            // Write to mmc-pack.json
+            // Don't indent the string line
+            string mmcPackJsonPath = Path.Combine(instancePath, "mmc-pack.json");
+            string mmcPackJsonContent = @"{
+    ""components"": [
+        {
+            ""cachedName"": ""LWJGL 3"",
+            ""cachedVersion"": ""3.3.1"",
+            ""cachedVolatile"": true,
+            ""dependencyOnly"": true,
+            ""uid"": ""org.lwjgl3"",
+            ""version"": ""3.3.1""
+        },
+        {
+            ""cachedName"": ""Minecraft"",
+            ""cachedRequires"": [
+                {
+                    ""suggests"": ""2.9.4-nightly-20150209"",
+                    ""uid"": ""org.lwjgl""
+                }
+            ],
+            ""cachedVersion"": ""1.7.10"",
+            ""important"": true,
+            ""uid"": ""net.minecraft"",
+            ""version"": ""1.7.10""
+        },
+        {
+            ""cachedName"": ""Forge-lwjgl3ify-Patches"",
+            ""cachedVersion"": ""1.4.0"",
+            ""uid"": ""me.eigenraven.lwjgl3ify.forgepatches""
+        },
+        {
+            ""cachedName"": ""Forge"",
+            ""cachedRequires"": [
+                {
+                    ""equals"": ""1.7.10"",
+                    ""uid"": ""net.minecraft""
+                }
+            ],
+            ""cachedVersion"": ""10.13.4.1614"",
+            ""uid"": ""net.minecraftforge"",
+            ""version"": ""10.13.4.1614""
+        }
+    ],
+    ""formatVersion"": 1
+}";
+            File.WriteAllText(mmcPackJsonPath, mmcPackJsonContent);
+        }
+
+        // This method updates the download progress bar and label with the download progress
+        private DateTime _lastUpdateTime = DateTime.MinValue;
+        private string _lastRemainingTime = "";
+
+        // This method updates the download progress bar and label with the download progress
+        private void UpdateProgress(long totalBytesRead, long? contentLength, Stopwatch stopwatch)
+        {
+            int percentComplete = 0;
+            if (contentLength.HasValue)
+            {
+                percentComplete = (int)((totalBytesRead * 1d) / (contentLength * 1d) * 100);
+            }
+            double bytesPerSecond = totalBytesRead / stopwatch.Elapsed.TotalSeconds;
+            string speed;
+            if (bytesPerSecond / 1024 / 1024 > 1)
+            {
+                speed = $"{bytesPerSecond / 1024 / 1024:F2} MB/s";
+            }
+            else
+            {
+                speed = $"{bytesPerSecond / 1024:F2} KB/s";
+            }
+            if (percentComplete > 100)
+                percentComplete = 100;
+            progressBar.Value = percentComplete;
+
+            // Update the progressLabel.Text only once every second
+            if ((DateTime.Now - _lastUpdateTime).TotalSeconds >= 1)
+            {
+                string progressText = $"{percentComplete}% ({Math.Min(totalBytesRead, contentLength.GetValueOrDefault()) / 1024 / 1024:F2} / {contentLength / 1024 / 1024:F2} MB), Download speed: {speed}, ";
+
+                TimeSpan remainingTime = TimeSpan.FromSeconds(3600);
+                if (contentLength.HasValue && bytesPerSecond > 0)
+                {
+                    long remainingBytes = contentLength.Value - totalBytesRead;
+                    double remainingSeconds = remainingBytes / bytesPerSecond;
+                    remainingTime = TimeSpan.FromSeconds(remainingSeconds);
+                    _lastRemainingTime = remainingTime.ToString(@"hh\:mm\:ss");
+                }
+                progressText += $" Time remaining: {_lastRemainingTime}";
+
+                progressLabel.Text = progressText;
+
+                _lastUpdateTime = DateTime.Now;
+            }
+        }
+
+        // This method extracts the contents of the downloaded file to the instance folder, deletes the downloaded file, and returns the path of the folder that was extracted from the jar file
+        private string ExtractDownloadedFile(string filePath)
+        {
+            string instancePath = Path.Combine(multiMCPathTextBox.Text, "instances");
+
+            using (Ionic.Zip.ZipFile zip = Ionic.Zip.ZipFile.Read(filePath))
+            {
+                long totalBytesToExtract = zip.Sum(entry => entry.UncompressedSize);
+                long totalBytesExtracted = 0;
+                Stopwatch stopwatch = Stopwatch.StartNew();
+
+                zip.ExtractProgress += (sender, e) =>
+                {
+                    if (e.EventType == ZipProgressEventType.Extracting_EntryBytesWritten)
+                    {
+                        totalBytesExtracted += e.BytesTransferred;
+                        UpdateProgress(totalBytesExtracted, totalBytesToExtract, stopwatch);
+                    }
+                };
+
+                zip.ExtractAll(instancePath);
+            }
+
+            // Ensure that all streams are properly disposed of before attempting to delete the file
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            File.Delete(filePath);
+
+            // Return the path of the folder that was extracted from the jar file
+            return Path.Combine(instancePath, "GT New Horizons " + GetSelectedModpackVersionNumber());
+        }
+
+        // Get selected modpack version number
+        private string? GetSelectedModpackVersionNumber()
+        {
+            string? version = versionListBox.SelectedItem.ToString();
+            if (version != null)
+            {
+                int index = version.IndexOf(" ");
+                if (index != -1)
+                {
+                    version = version.Substring(0, index);
+                }
+                return version;
+            }
+            return null;
         }
     }
 }
