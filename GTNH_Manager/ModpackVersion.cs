e6172d5553d8bb2d4b5aff8b156e1442c28273a6
Nhat <60876500+UpperMoon0@users.noreply.github.com>
2023-12-12 23:21:37 +0700

    2.4.1

--- GTNH_Manager/ModpackVersion.cs
+++ GTNH_Manager/ModpackVersion.cs
@@ -6,6 +6,9 @@
         private const string optifineDownloadUrl = "https://drive.google.com/u/0/uc?id=1nR5ob2lm9RTMiczTlKiq5jCvvIFrhM_7&export=download";
         static ModpackVersion()
         {
+            urlDict.Add("2.4.1", "http://downloads.gtnewhorizons.com/Multi_mc_downloads/betas/GT_New_Horizons_2.4.1_Java_17-20.zip");
+            urlDict.Add("2.4.0", "http://downloads.gtnewhorizons.com/Multi_mc_downloads/GT_New_Horizons_2.4.0_Java_17-20.zip");
+            urlDict.Add("2.3.7", "https://www.dropbox.com/scl/fi/hgtlp7totzmkhr7zz4mqn/GT_New_Horizons_2.3.7_Java_17-20.zip?dl=1&rlkey=4zaz5gvrsw01agm5bbcg5twar");
             urlDict.Add("2.3.6", "https://dl.dropboxusercontent.com/scl/fi/qhtjdjoolozofjht2qrks/GT_New_Horizons_2.3.6_Client_Java_17-20.zip?dl=0&rlkey=z58bagc14k6ij7bxdhrue9sex");
             urlDict.Add("2.3.5", "https://www.dropbox.com/scl/fi/r5ohaxwfzvyrphx2bn6zj/GT_New_Horizons_2.3.5_Client_Java_17-20.zip?dl=1&rlkey=spfjszkg02nrej3eqds42r0cg");
             urlDict.Add("2.3.4", "https://dl.dropboxusercontent.com/s/a58kvuhbpiuvey8/GT_New_Horizons_2.3.4_Java_17-20.zip");
--- GTNH_Manager/obj/Release/net6.0-windows/GTNH_Manager.AssemblyInfo.cs
+++ GTNH_Manager/obj/Release/net6.0-windows/GTNH_Manager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GTNH_Manager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+720937a095db05559b248d8e64a6b9ff0223297f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GTNH_Manager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GTNH_Manager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
